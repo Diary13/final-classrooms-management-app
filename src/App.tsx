7fc6a556ad7e160b5560add4eb9f8f17704bb6f1
ainaaris <daxniayn@gmail.com>
2021-12-11 06:18:11 +0100

    Add package

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { HomeClient } from './pages/login/client/home/home';
 import Login from './pages/login/login';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/client/home' element={<HomeClient />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
