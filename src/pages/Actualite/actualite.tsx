009ac8c3e83279df3ce0d7373836e10dacf2b8ee
ainaaris <daxniayn@gmail.com>
2021-12-11 06:39:39 +0100

    Config Routes

---
+++ src/pages/Actualite/actualite.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../actualite.css';
+
+const actualite = () => {
+    return (
+        <div>
+            <h1>Yes</h1>
+        </div>
+    );
+};
+
+export default actualite;
